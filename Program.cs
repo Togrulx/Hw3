465416eefa41d18ace66135fe06a703e016e62a7
Togrul Talibov <togrultalibov@Togrul-MacBook-Pro.local>
2022-02-10 11:24:53 +0400

    Hw3 commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hw3.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
